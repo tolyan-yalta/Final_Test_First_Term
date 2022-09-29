534548fc38d8439b0a916a96ac5ccebbaf1c59fb
anatoly <tolyan.yalta@yandex.ru>
2022-09-29 12:26:51 +0300

    Final_Test version 1.0. Added array with lines

--- Final_Test/Program.cs
+++ Final_Test/Program.cs
@@ -5,8 +5,36 @@
 // При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+string FillLine(int max)    // Генерация строки длиной от 1 до max состоящей из случайных символов Основной латиницы в Юникоде
+{
+    int number = new Random().Next(1, max);
+    string temp;
+    string line = String.Empty;
+    for (int i = 0; i < number; i++)
+    {
+        temp = line;
+        line = temp + (char)new Random().Next(0x0021, 0x007E);
+    }
+    return line;
+}
 
 
+
+
+int max = 10;
+int length = 5;
+
+string[] lines = new string[length];
+
+for (int i = 0; i < length; i++)
+{
+    lines[i] = FillLine(max);
+}
+
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine($"\t{lines[i]}");
+}
 
 
 
